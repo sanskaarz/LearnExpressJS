6bf87720f3cc5a55bf76c407029b0584a4cb5bf5
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-10-06 10:45:42 +0530

    Refactor root path response and add new route for dynamic username and id
    In index.js, the code has been refactored to change the response for the root path from "Hello World!" to "This is a root path". Additionally, a new route has been added that accepts dynamic parameters for username and id, and responds with a customized message. The console will log "request received" when this route is accessed.

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ let port = 3000;
 //   });
 
 app.get("/", (req, res) => {
-    res.send('Hello World!')
+    res.send('This is a root path')
   })
-app.get("/search", (req, res) => {
-    res.send("Let's search")
-}) 
-app.get("/contact", (req, res) => {
-    res.send("Let's connect")
-})
-app.get("*", (req, res) => { // Default only works when the path is incorrect.
-    res.send("This path does not exist")
-})
+// app.get("/search", (req, res) => {
+//     res.send("Let's search")
+// }) 
+// app.get("/contact", (req, res) => {
+//     res.send("Let's connect")
+// })
+// app.get("*", (req, res) => { // Default only works when the path is incorrect.
+//     res.send("This path does not exist")
+// })
+
+
+app.get("/:username/:id" , (req , res) => {
+    let{ username, id } = req.params;
+    res.send(`hello, Sanskar Over here${username}`);
+    console.log("request reved")
+});
 
 app.listen(port, () => {
         console.log(`app is listening on ${port}`);
