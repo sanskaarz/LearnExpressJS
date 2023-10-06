c1cae1af4b06b89f23c3909f420ea4a4f4b73ab8
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-10-06 16:22:11 +0530

    Add request logging and a default route handler
    - Add request logging using the `app.use` middleware to log the method and URL of each request.
    - Create a default route handler for paths that don't match any defined routes.
    - Improve code formatting and remove commented out code.
    - Add console logs for connect request and invalid path.
    
    This commit adds request logging and a default route handler to improve the functionality and debugging experience of the application.

--- index.js
+++ index.js
@@ -3,42 +3,35 @@ const app = express();
 
 let port = 3000;
 
-// app.use((req, res,) => {
-    //     console.log(`Request received: ${req.method} ${req.url}`);
-    //     res.send("Hello ji, Sanskar Over hereeeee")
-    //   });
+app.use((req, res,) => {
+        console.log(`Request received: ${req.method} ${req.url}`);
+        res.send("Hello ji, Sanskar Over hereeeee")
+});
     
 app.get("/", (req, res) => {
     res.send('This is a roott path')
-})
-// app.get("/search", (req, res) => {
-    //     res.send("Let's search")
-    // }) 
+});
+
+app.get("/search", (req, res) => {
+        res.send("Let's search")
+});
+
 app.get("/contact", (req, res) => {
     console.log("Connect request")
     res.send("Let's connect")
-})
-// app.get("*", (req, res) => { // Default only works when the path is incorrect.
-//     res.send("This path does not exist")
-// })
-
-// app.get("/:username", (req, res) => {
-//     console.log(req.params)
-//     let {username}= req.params;
-// })
-
-app.get("/:username", (req, res) => {
-    console.log(req.params)
-    let { username } = req.params;
-    res.send(`welcome ${username}`)
 });
 
+app.get("/user/:username/:id" , (req , res) => {
+    let{ username, id } = req.params;
+    res.send(`welcome ${username}! Your id is ${id}`);
+    console.log("request recieved")
+    console.log(req.params)
+});
 
-// app.get("/:username/:id" , (req , res) => {
-//     let{ username, id } = req.params;
-//     res.send(`hello, Sanskar Over here${username}`);
-//     console.log("request recieved")
-// });
+app.get("*", (req, res) => { // Default only works when the path is incorrect.
+    res.send("This path does not exist")
+    console.log("invalid path")
+});
 
 
 app.listen(port, () => {
