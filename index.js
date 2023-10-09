fb8a88896a7cabaac4282056df9848e7c29fd2da
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-10-09 16:03:35 +0530

    Refactor routes and add request logging
    - Refactor "/search" route to "/find"
    - Add logging for request method and URL for each request
    - Print request parameters for "/user/:username/:id" route
    - Add new "/search" route to handle search queries
    - Log invalid paths with "invalid path" message
    - Update server listening port to 8080
    
    Refactor routes and add request logging
    
    - Refactor "/search" route to "/find"
    - Add logging for request method and URL for each request
    - Print request parameters for "/user/:username/:id" route
    - Add new "/search" route to handle search queries
    - Log invalid paths with "invalid path" message
    - Update server listening port to 8080
    
    Refactor routes and add request logging
    
    - Refactor "/search" route to "/find"
    - Add request logging for each request method and URL
    - Print request parameters for "/user/:username/:id" route
    - Add new "/search" route to handle search queries
    - Log invalid paths with "invalid path" message
    - Update server listening port to 8080

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ const app = express();
 
 let port = 3000;
 
+app.use((req, res,next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send('This is a roott path')
     console.log("root request")
 });
 
-app.get("/search", (req, res) => {
+app.get("/find", (req, res) => {
     console.log("Search request")
-    res.send("Let's search")
+    res.send("What to find")
 });
 
 app.get("/contact", (req, res) => {
     console.log("Connect request")
     res.send("Let's connect")
+    console.log(req.params)
 });
 
 app.get("/user/:username/:id" , (req , res) => {
     let{ username, id } = req.params;
     res.send(`welcome ${username}! Your id is ${id}`);
     console.log("request recieved")
-    console.log(req.params)
+});
+
+app.get("/search" , (req,res) =>{
+    let {q} = req.query
+    console.log(req.query)
+    if(!q){
+        res.send("No Search Query")
+    }
+
+    res.send(`These are the results for your query: ${q}`)
 });
 
 app.get("*", (req, res) => { // Default only works when the path is incorrect.
@@ -30,12 +45,6 @@ app.get("*", (req, res) => { // Default only works when the path is incorrect.
     console.log("invalid path")
 });
 
-app.use((req, res,next) => {
-    console.log(`Request received: ${req.method} ${req.url}`);
-    res.send("Hello ji, Sanskar Over hereeeee");
-    next();
-});
-
-app.listen(3000, () => {
+app.listen(8080, () => {
         console.log(`app is listening on ${port}`);
 });
